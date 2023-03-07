3d0e0a962154e3a1c47217b9f99623992e911c5b
Magesh Kuppan <tkmagesh77@gmail.com>
2023-03-08 01:35:44 +0530

    updates

--- js-language/sum.js
+++ js-language/sum.js
@@ -1,5 +1,19 @@
 function sum(x,y){
-    return x + y;
+    function parseArg(n){
+        if (Array.isArray(n)){
+            var result = 0;
+            for (var i = 0; i < n.length; i++){
+                result += parseArg(n[i])
+            }
+            return result
+        }
+        return isNaN(n) ? 0 : parseInt(n)
+    }
+    var result = 0
+    for (var i = 0; i < arguments.length; i++){
+        result += parseArg(arguments[i])
+    }
+    return result;
 }
 
 module.exports = sum;
--- js-language/sum.test.js
+++ js-language/sum.test.js
@@ -1,14 +1,50 @@
 var sum = require('./sum');
 
-test('adding 2 numbers', function(){
+test('sum(10,20) => 30', function(){
     //arrange
-    var x = 100,
-        y = 200,
-        expectedResult = 300
+    var x = 10,
+        y = 20,
+        expectedResult = 30
 
     //act
     var actualResult = sum(x,y);
 
     //assert
+    expect(actualResult).toBe(expectedResult)
+})
+
+test('sum(10, "20") => 30', function(){
+    var expectedResult = 30
+    var actualResult = sum(10, "20")
+    expect(actualResult).toBe(expectedResult)
+})
+
+test('sum(10, "abc") => 10', function(){
+    var expectedResult = 10
+    var actualResult = sum(10, "abc")
+    expect(actualResult).toBe(expectedResult)
+})
+
+test('sum(10,20,30,40,50) => 150', function(){
+    var expectedResult = 150
+    var actualResult = sum(10, 20, 30, 40, 50)
+    expect(actualResult).toBe(expectedResult)
+})
+
+test('sum() => 0', function(){
+    var expectedResult = 0
+    var actualResult = sum()
+    expect(actualResult).toBe(expectedResult)
+})
+
+test('sum([10,20,30],[40,50,60]) => 210', function(){
+    var expectedResult = 210
+    var actualResult = sum([10, 20, 30], [40, 50, 60])
+    expect(actualResult).toBe(expectedResult)
+})
+
+test('sum([10,"20",30],[40,50,"abc"]) => 150', function(){
+    var expectedResult = 150
+    var actualResult = sum([10, "20", 30], [40, 50, "abc"])
     expect(actualResult).toBe(expectedResult)
 })
