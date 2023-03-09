df7668d68ae9e4bcc4ea46ab4f1cef918938aaf2
Magesh Kuppan <tkmagesh77@gmail.com>
2023-03-10 04:15:30 +0530

    using promises & async-await

--- async-programming/async-demos.js
+++ async-programming/async-demos.js
@@ -37,24 +37,95 @@
 
     //Using promise
     function addAsyncPromise(x, y) {
-        console.log(`   [@service] processing ${x} and ${y}`)
+        console.log(`   [@add service] processing ${x} and ${y}`)
         const p = new Promise(function(resolveFn, rejectFn){
             setTimeout(() => {
                 const result = x + y
-                console.log(`   [@service] returning result`)
+                console.log(`   [@add service] returning result`)
                 resolveFn(result)
             }, 5000)
         })
         return p
     }
 
+    window['addAsyncPromise'] = addAsyncPromise
+
+    function divideAsyncPromise(x, y) {
+        console.log(`   [@divide service] processing ${x} and ${y}`)
+        const p = new Promise(function (resolveFn, rejectFn) {
+            setTimeout(() => {
+                if (y === 0){
+                    rejectFn(new Error('divisor cannot be 0'))
+                }
+                const result = x / y
+                console.log(`   [@divide service] returning result`)
+                resolveFn(result)
+            }, 3000)
+        })
+        return p
+    }
+
+    window['divideAsyncPromise'] = divideAsyncPromise
+
+    /* 
     function addAsyncPromiseClient(x,y){
         console.log(`[@client] invoking the service`)
         var p = addAsyncPromise(x, y)
-        p.then(function (result) {
+        return p.then(function (result) {
             console.log(`[@client] result = ${result}`)
         })
+    } 
+    */
+
+    async function addAsyncPromiseClient(x, y) {
+        console.log(`[@client] invoking the service`)
+        var result = await addAsyncPromise(x, y)
+        console.log(`[@client] result = ${result}`)
     }
 
-    window['addAsyncPromiseClient'] = addAsyncPromiseClient
+    window['addAsyncPromiseClient'] = addAsyncPromiseClient 
+
+    /* 
+    async function processNos(x,y){
+        const addResult = await addAsyncPromise(x,y)
+        const divideResult = await divideAsyncPromise(x,y)
+        console.log('addResult :', addResult)
+        console.log('divideResult :', divideResult)
+    } 
+    */
+
+    function processNos(x,y){
+        var p1 = addAsyncPromise(x, y)
+        var p2 = divideAsyncPromise(x, y)
+        Promise.all([p1, p2])
+            .then(([addResult, divideResult]) => {
+                console.log(`add result : ${addResult}`)
+                console.log(`divide result : ${divideResult}`)
+            }) 
+    }
+    window['processNos'] = processNos
+   
 })()
+
+// Using Promise.all()
+/* 
+let x = 100, y = 20
+var p1 = addAsyncPromise(x, y)
+var p2 = divideAsyncPromise(x, y)
+Promise.all([p1, p2])
+    .then(([addResult, divideResult]) => {
+        console.log(`add result : ${addResult}`)
+        console.log(`divide result : ${divideResult}`)
+    }) 
+*/
+
+// Using Promise.any()
+/* 
+let x = 100, y = 20
+var p1 = addAsyncPromise(x,y)
+var p2 = divideAsyncPromise(x,y)
+Promise.any([p1, p2])
+    .then((result) => {
+        console.log(` result : ${result}`)
+    })
+*/
